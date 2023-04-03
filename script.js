9ae06aa9ac5c91a4737ec84d08246fd1701218f9
nawzaysfinah <nawzaysfinah@gmail.com>
2023-04-03 12:24:21 +0800

    Initial set up of index.html and script.js for RPS

---
+++ script.js
@@ -0,0 +1,7 @@
+// Write function that plays a single round of RPS. It should take two parameters: playerSelection, computerSelection
+let playerSelection;
+let computerSelection;
+
+// Return a string that declares the winner of the round "You Lose! 'Paper' beats 'Rock' "
+// Make your function's playerSelection parameter case-insensitive
+playerSelection = playerSelection.toUpperCase();
